a80feeb16e2717e72709088debe2e65c25617607
nathwyatt <nathwyatt0@gmail.com>
2024-10-15 08:55:07 +0200

    added new class in config/app

--- config/app.php
+++ config/app.php
@@ -184,6 +184,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
 
         /*
          * Package Service Providers...
@@ -213,6 +215,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
     ])->toArray(),
 
 
